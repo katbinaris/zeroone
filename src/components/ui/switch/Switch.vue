7769280e9b1056e0ab1b042399262ef03d13cbee
Robert Kossessa <github@kosro.de>
2024-01-25 01:41:01 +0100

    UPD: Improve switch visibility

--- src/components/ui/switch/Switch.vue
+++ src/components/ui/switch/Switch.vue
@@ -24,7 +24,7 @@ const forwarded = useForwardPropsEmits(props, emits);
     v-bind="forwarded"
     :class="
       cn(
-        'peer inline-flex h-[20px] w-[36px] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input',
+        'peer inline-flex h-[20px] w-[36px] shrink-0 cursor-pointer items-center rounded-full border-2 data-[state=checked]:border-transparent data-[state=unchecked]:border-zinc-800 shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-zinc-100 data-[state=unchecked]:bg-black',
         props.class
       )
     "
@@ -32,7 +32,7 @@ const forwarded = useForwardPropsEmits(props, emits);
     <SwitchThumb
       :class="
         cn(
-          'pointer-events-none block h-4 w-4 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0'
+          'pointer-events-none block h-4 w-4 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0 data-[state=unchecked]:bg-zinc-300'
         )
       "
     />
