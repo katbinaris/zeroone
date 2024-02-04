4729a0029c727cb98d0ed49fe002d83364a90c23
Robert Kossessa <github@kosro.de>
2024-02-04 01:58:33 +0100

    UPD: Slider transitions

--- src/components/ui/slider/Slider.vue
+++ src/components/ui/slider/Slider.vue
@@ -38,12 +38,12 @@ const emits = defineEmits(['update:modelValue', 'valueCommit'])
     v-bind="{ ...props, ...useEmitAsProps(emits) }"
   >
     <SliderTrack
-      class="flex h-2 w-full grow overflow-hidden rounded-full bg-primary/20 items-center"
+      class="flex h-2 w-full grow overflow-hidden rounded-full bg-primary/20 items-center transition-all duration-75"
     >
       <SliderRange class="absolute h-1.5 bg-primary rounded-full" />
     </SliderTrack>
     <SliderThumb
-      class="flex h-6 w-8 rounded-[8px] hover:bg-zinc-200 outline outline-zinc-100 bg-zinc-300 transition-colors focus-visible:outline-none focus-visible:ring-1 cursor-pointer focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 text-zinc-600 justify-center items-center"
+      class="flex h-6 w-8 rounded-[8px] hover:bg-zinc-200 outline outline-zinc-100 bg-zinc-300 transition-all duration-75 focus-visible:outline-none focus-visible:ring-1 cursor-pointer focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 text-zinc-600 justify-center items-center"
       style="box-shadow: -3px 0 15px 0 rgba(0,0,0,0.6)">
       <MoreHorizontal class="h-full" />
     </SliderThumb>
