4631b0197872fb7377897ce1c3a89d81ceee3ef8
Robert Kossessa <github@kosro.de>
2024-02-06 21:01:15 +0100

    FIX: Disconnect panel hide

--- src/App.vue
+++ src/App.vue
@@ -14,14 +14,18 @@ store.fetchProfiles()
   <main class="select-none w-screen h-screen flex flex-col">
     <Navbar class="flex-none" />
     <div class="flex-1 min-h-0 flex flex-row justify-center">
-      <ProfileManager
-        :class="{'invisible': !store.connected }"
-        class="basis-1/3 min-w-60 flex-1 flex flex-col border-solid border-0 border-r bg-zinc-900 bg-opacity-50" />
+      <div class="basis-1/3 min-w-60 flex-1 flex">
+        <ProfileManager
+          v-if="store.connected"
+          class="flex-1 flex flex-col border-solid border-0 border-r bg-zinc-900 bg-opacity-50" />
+      </div>
       <DevicePreview
         class="basis-1/3 flex-col flex" />
-      <ConfigPane
-        :class="{'invisible': !store.connected }"
-        class="flex-1 basis-2/5 flex flex-col border-solid border-0 border-l bg-zinc-900 bg-opacity-50" />
+      <div class="basis-2/5 flex-1 flex">
+        <ConfigPane
+          v-if="store.connected"
+          class="flex-1 flex flex-col border-solid border-0 border-l bg-zinc-900 bg-opacity-50" />
+      </div>
     </div>
   </main>
 </template>
