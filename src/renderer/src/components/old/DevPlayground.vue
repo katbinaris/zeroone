ff891dfab97e928c4d6f0057169df68ee2451d8f
Robert Kossessa <github@kosro.de>
2024-03-02 18:24:53 +0100

    DEL: Old components

--- src/renderer/src/components/old/DevPlayground.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <WIP />
-  <DeviceLEDRing />
-</template>
-<script setup>
-import WIP from '@renderer/components/WIP.vue'
-import DeviceLEDRing from '@renderer/components/device/DeviceLEDRing.vue'
-</script>
