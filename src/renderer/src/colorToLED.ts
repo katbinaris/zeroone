bc3caf7764ca511b36350cbe83cd8ce4e0eb26d3
Robert Kossessa <github@kosro.de>
2024-05-20 16:28:09 +0200

    FIX: LED Colors (in hsv now)

--- src/renderer/src/colorToLED.ts
+++ src/renderer/src/colorToLED.ts
@@ -2,8 +2,8 @@ import Color from 'color'
 
 export const colorToLED = (color: Color) => {
   if (!color) return null
-  const l = color.lightness()
-  if (l < 0.5) return color.lightness(0)
-  const nl = 1 - Math.pow(l / 50 - 1, 4)
-  return color.lightness(nl * 35 + 20)
+  const l = color.value()
+  if (l < 1) return color
+  const nl = 1 - Math.pow(l / 100 - 1, 2)
+  return color.value(nl * 70 + 30)
 }
