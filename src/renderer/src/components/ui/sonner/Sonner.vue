a8af352a70149de4b6f6b5031d86d3da4d332a5d
Robert Kossessa <github@kosro.de>
2024-05-28 12:49:57 +0200

    UPD: Style sonner

--- src/renderer/src/components/ui/sonner/Sonner.vue
+++ src/renderer/src/components/ui/sonner/Sonner.vue
@@ -10,13 +10,12 @@ const props = defineProps<ToasterProps>()
     v-bind="props"
     :toast-options="{
       classes: {
-        toast: 'group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg',
-        description: 'group-[.toast]:text-muted-foreground',
-        actionButton:
-          'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground',
-        cancelButton:
-          'group-[.toast]:bg-muted group-[.toast]:text-muted-foreground',
-      },
+        toast:
+          'group toast group-[.toaster]:bg-background font-heading group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg',
+        description: 'group-[.toast]:text-muted-foreground font-mono',
+        actionButton: 'group-[.toast]:bg-primary group-[.toast]:text-primary-foreground',
+        cancelButton: 'group-[.toast]:bg-muted group-[.toast]:text-muted-foreground'
+      }
     }"
   />
 </template>
